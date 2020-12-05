45b325d60da6b2c839d144a4ddd3a8222ec0365b
noah <71029726+filedao@users.noreply.github.com>
2020-12-05 16:45:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -154,7 +154,7 @@ clear() {
 
 git_clone() {
     # filecash/v0.9.0
-    source $CLONE_AND_CHECKOUT "https://github.com/filecash/lotus.git" lotus "8e363648328a7718be20a3ceb82dcdcf781b72ab"
+    source $CLONE_AND_CHECKOUT "https://github.com/filecash/lotus.git" lotus "a75a68217e60fddb313ebe44bbdf55c7bf55e71e"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/rust-filecoin-proofs-api.git" rust-filecoin-proofs-api "f704653ab77246e2982251d44136ca1c20b497ce"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/rust-fil-proofs.git" rust-fil-proofs "c8cfc7d2868ffbf05fb0dd0a7c2d6e68b2a06702"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/specs-actors.git" specs-actors-v0.9.12 "2e5925f8069e5cff89233cbbb5b7817cb2a9fac8"
