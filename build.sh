464bad3feee8d49ac24fd80eecd890fe3ffcb160
wonderfic <73159371+wonderfic@users.noreply.github.com>
2020-12-21 11:34:18 +0800

    fix passwd

--- build.sh
+++ build.sh
@@ -154,7 +154,7 @@ clear() {
 
 git_clone() {
     # filecash/v0.9.0
-    source $CLONE_AND_CHECKOUT "https://github.com/filecash/lotus.git" lotus "6bf613e7d1783c911d387e643939e033447a2dce"
+    source $CLONE_AND_CHECKOUT "https://github.com/filecash/lotus.git" lotus "467d6e1448d0a91a2e2d7159f37a2164b519d79e"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/rust-filecoin-proofs-api.git" rust-filecoin-proofs-api "f704653ab77246e2982251d44136ca1c20b497ce"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/rust-fil-proofs.git" rust-fil-proofs "c8cfc7d2868ffbf05fb0dd0a7c2d6e68b2a06702"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/specs-actors.git" specs-actors-v0.9.12 "2e5925f8069e5cff89233cbbb5b7817cb2a9fac8"
