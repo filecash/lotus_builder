80b916b64980368d78dc78ad27ce37f8b24c7b2a
chenchao <chenchao2014@live.com>
2021-03-03 14:31:07 +0800

    modify path error

--- select_target.sh
+++ select_target.sh
@@ -7,22 +7,22 @@ if [ "$is_amd" != "" ]; then
     echo "AMD"
     if [ "$have_avx2" != "" ]; then
         echo "use avx2"
-        cp template/sha2raw/Cargo_avx2.toml rust-fil-proofs/sha2raw/Cargo.toml
+        cp sha2raw/Cargo_avx2.toml rust-fil-proofs/sha2raw/Cargo.toml
     else
         echo "use asm"
-        cp template/sha2raw/Cargo_asm.toml rust-fil-proofs/sha2raw/Cargo.toml
+        cp sha2raw/Cargo_asm.toml rust-fil-proofs/sha2raw/Cargo.toml
     fi
 elif [ "$is_intel" != "" ]; then
     echo "INTEL"
     if [ "$have_avx2" != "" ]; then
         echo "use avx2"
-        cp template/sha2raw/Cargo_avx2.toml rust-fil-proofs/sha2raw/Cargo.toml
+        cp sha2raw/Cargo_avx2.toml rust-fil-proofs/sha2raw/Cargo.toml
     elif [ "$have_avx" != "" ]; then
         echo "use avx"
-        cp template/sha2raw/Cargo_avx.toml rust-fil-proofs/sha2raw/Cargo.toml
+        cp sha2raw/Cargo_avx.toml rust-fil-proofs/sha2raw/Cargo.toml
     else
         echo "use asm"
-        cp template/sha2raw/Cargo_asm.toml rust-fil-proofs/sha2raw/Cargo.toml
+        cp sha2raw/Cargo_asm.toml rust-fil-proofs/sha2raw/Cargo.toml
     fi
 else
     echo "UNKNOW"
