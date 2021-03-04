d41e6efaeba72b90cbf5d5ed5b7a3e79547dec65
陈江宏 <kentin.chen@gmail.com>
2021-03-04 18:08:08 +0800

    add build features
    fix commit err

--- build.sh
+++ build.sh
@@ -224,7 +224,7 @@ git_clone() {
 
     # filecash/v1.2.2
 
-    source $CLONE_AND_CHECKOUT "https://github.com/filecash/lotus.git" lotus "a58837a0efde9df72b11352506e84d2f15bb4e"
+    source $CLONE_AND_CHECKOUT "https://github.com/filecash/lotus.git" lotus "cba58837a0efde9df72b11352506e84d2f15bb4e"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/filecoin-ffi.git" filecoin-ffi "20771c8dec42211bb7cd618ce474bd6aea81e36c"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/rust-filecoin-proofs-api.git" rust-filecoin-proofs-api "5e8c7b2143656405e7d56f585233493de9342544"
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/rust-fil-proofs.git" rust-fil-proofs "4178c7994617cf81ff540e7e98c345cc603c4af0"
