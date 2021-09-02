2800cb21913bbfc27b578f87f7d1c408e3a25c26
wonderfic <wonderfic@gmail.com>
2021-09-02 11:41:39 +0800

    wdpost&wnpost parallel calc

--- build.sh
+++ build.sh
@@ -240,8 +240,8 @@ git_clone() {
     # filecash/0.21.0 17b0ac21d13e85f76e474e45db24d95181265781
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/merkletree.git" merkletree "filecash/0.21.0"
 
-    # filecash/v1.2.2 26b53042c1689d4c0ccb163a44ab0ac60a192ef4
-    source $CLONE_AND_CHECKOUT "https://github.com/filecash/bellman.git" bellperson "fa45a463ebf267751857ac99c89fe5553a9315d4"
+    # filecash/v1.2.2 eb4d1a08d4c38eff9e44ad7e901d36e8221e3cec
+    source $CLONE_AND_CHECKOUT "https://github.com/filecash/bellman.git" bellperson "eb4d1a08d4c38eff9e44ad7e901d36e8221e3cec"
     # filecash/v1.2.2 37caaecccf5ab6cb6bd595f97b96ad12fae4db3b
     # filecash/v1.5.0 5d55341d9ad26c3cd8e8887d6d066c26365216e8
     source $CLONE_AND_CHECKOUT "https://github.com/filecash/neptune.git" neptune "5d55341d9ad26c3cd8e8887d6d066c26365216e8"
